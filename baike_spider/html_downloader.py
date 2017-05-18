93eaddcaf4b00e886bc13c3dd4c4652eab77157c
Xin Guan <guanxin0206@gmail.com>
2017-05-18 14:46:03 -0700

    added all files

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python -t
+# -*- coding: UTF-8 -*-
+import urllib2
+
+class HtmlDownloader(object):
+    def download(self,url):
+        if url is None:
+            return None
+
+        else:
+            response = urllib2.urlopen(url)
+        if response.getcode() != 200:
+            return None
+
+        return response.read()
+
